e5c8c98236f2e18db05a72876650525393ef3d00
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-16 15:24:13 -0700

    Add 'init' and 'send' methods. Implement 'send'

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,18 @@
-// YOUR CODE HERE:
-
+var app = {
+  init: function () {},
+  send: function (message, type) {
+    type = type || 'POST';
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: type,
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function(data) {
+        console.log('chatterbox: Message sent.');
+      },
+      error: function (data) {
+        console.log('chatterbox: Failed to send message.');
+      }
+    });
+  }
+};
